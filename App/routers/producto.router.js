3c9191dff1c2be5a73a8690e61d107428a6d2240
jreyesc22 <jreyesc22@miumg.edu.gt>
2024-11-23 10:06:19 -0600

    correcionrutas

--- App/routers/producto.router.js
+++ App/routers/producto.router.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const productos = require('../controllers/producto.controller.js');
 
 // Crear un nuevo producto
-router.post('/productos', productos.create);
+router.post('/crear', productos.create);
 
 // Recuperar todos los productos
-router.get('/productos', productos.findAll);
+router.get('/all', productos.findAll);
 
 // Recuperar un solo producto por ID
-router.get('/productos/:id', productos.findOne);
+router.get('obtener/:id', productos.findOne);
 
 // Actualizar un producto por ID
-router.put('/productos/:id', productos.update);
+router.put('actualizar/:id', productos.update);
 
 // Eliminar un producto por ID
-router.delete('/productos/:id', productos.delete);
+router.delete('/eliminar/:id', productos.delete);
 
 module.exports = router;
