798cf727dd9ac13b29152f1f097dc905a0428ebf
jreyesc22 <jreyesc22@miumg.edu.gt>
2024-11-26 14:15:42 -0600

    cons

--- App/models/producto.model.js
+++ App/models/producto.model.js
@@ -2,39 +2,28 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
   const Medicamento = sequelize.define('Medicamento', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
     idProducto: {
       type: DataTypes.STRING,
-      allowNull: false, // Campo obligatorio
-      unique: true, // Debe ser único
     },
     nombre: {
       type: DataTypes.STRING,
-      allowNull: false, // Campo obligatorio
-      trim: true, // Elimina espacios en blanco (manejado en frontend/back)
     },
     precio: {
-      type: DataTypes.DECIMAL(10, 2), // Manejo de decimales con precisión
-      allowNull: false, // Campo obligatorio
-      validate: {
-        isDecimal: true, // Asegura que sea un número decimal
-        min: 0, // El precio debe ser positivo
-      },
+      type: DataTypes.DECIMAL(10, 2),
     },
     descripcion: {
       type: DataTypes.TEXT,
-      allowNull: true, // Campo opcional
     },
     fecha: {
-      type: DataTypes.DATEONLY, // Fecha en formato yyyy-mm-dd
-      allowNull: false, // Campo obligatorio
-      validate: {
-        isDate: true, // Valida que sea una fecha válida
-      },
+      type: DataTypes.DATEONLY,
     },
     statusPago: {
-      type: DataTypes.ENUM('pagado', 'pendiente'), // Opciones limitadas
-      allowNull: true, // Campo opcional
-      defaultValue: 'pendiente', // Valor por defecto
+      type: DataTypes.ENUM('pagado', 'pendiente'),
     },
   });
 
